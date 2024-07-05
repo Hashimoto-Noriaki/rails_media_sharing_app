34eded1b8f7111b8a1b5156d07566e4aadaee123
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-06 02:16:02 +0900

    timezoneの設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RailsMediaSharingApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Set the default time zone
+    config.time_zone = 'Tokyo'
   end
 end
