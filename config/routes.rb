9fce179cb95f7a7ec560215045d7f564781db3c1
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-13 01:26:33 +0900

    ユーザー登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :users
   root 'users#index'
+  devise_for :users
+  # , controllers: {
+  #   registrations: 'users/registrations'
+  #   # sessions: 'users/sessions'
+  # }
+  # Custom routes for user registration
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'signup', to: 'users#create', as: 'signup_post'
 end
