f6eebcc9cbe6fd9607855ce41a90583132a78294
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-13 01:29:04 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 255 }
+  #URI::MailTo::EMAIL_REGEXPは、RubyのライブラリのURIモジュールに定義されている正規表現。メールアドレスの形式を検証するために使用
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }, confirmation: true
 end
