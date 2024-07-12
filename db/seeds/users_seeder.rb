8f2fccc91ab4a84306bacca9ef48928a31c2ad52
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-12 19:33:00 +0900

    RuboCopの修正

--- db/seeds/users_seeder.rb
+++ db/seeds/users_seeder.rb
@@ -2,7 +2,7 @@
   User.create(
     name: "test#{val + 1}",
     email: "test#{val + 1}@example.com",
-    password_digest: "password#{val +1}",
+    password_digest: "password#{val + 1}",
     created_at: Time.zone.now
   )
 end
