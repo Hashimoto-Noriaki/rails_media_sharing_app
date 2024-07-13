59ac69293dc85551a386dacfa8d08fd7aab0a8b5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-13 23:55:51 +0900

    Railsの日本語化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if User.exists?(email: @user.email)
-      flash[:error] = 'このメールアドレスはすでに登録されています。'
+      flash[:error] = t('email_already_registered')
       render 'new'
     elsif @user.save
-      redirect_to root_path, notice: 'ユーザーが正常に登録されました。'
+      redirect_to root_path, notice: t('user_created_successfully')
     else
       render 'new'
     end
